178c722fb01b49d65aa3f9ab20f14e7d9e42e6da
Furore <106108202+delinquenst@users.noreply.github.com>
2022-10-15 08:06:45 +0100

    Update and rename ui.lua to awdawdawd.lua

--- awdawdawd.lua
+++ awdawdawd.lua
@@ -1,3 +1,5 @@
+-- Credit to material.
+
 local Player = game:GetService("Players").LocalPlayer
 local Mouse = Player:GetMouse()
 
@@ -170,7 +172,6 @@ local Themes = {
 		TextFieldAccent = Color3.fromRGB(255,255,255),
 	}
 }
-
 local Types = {
 	"RoundFrame",
 	"Shadow",
@@ -204,21 +205,21 @@ local ActualTypes = {
 local Properties = {
 	RoundFrame = {
 		BackgroundTransparency = 1,
-		Image = "http://www.roblox.com/asset/?id=5554237731",
+		Image = "http://www.roblox.com/asset/?id=11277856425",
 		ScaleType = Enum.ScaleType.Slice,
 		SliceCenter = Rect.new(3,3,297,297)
 	},
 	SmoothButton = {
 		AutoButtonColor = false,
 		BackgroundTransparency = 1,
-		Image = "http://www.roblox.com/asset/?id=5554237731",
+		Image = "http://www.roblox.com/asset/?id=11277856425",
 		ScaleType = Enum.ScaleType.Slice,
 		SliceCenter = Rect.new(3,3,297,297)
 	},
 	Shadow = {
 		Name = "Shadow",
 		BackgroundTransparency = 1,
-		Image = "http://www.roblox.com/asset/?id=5554236805",
+		Image = "http://www.roblox.com/asset/?id=11277861255",
 		ScaleType = Enum.ScaleType.Slice,
 		SliceCenter = Rect.new(23,23,277,277),
 		Size = UDim2.fromScale(1,1) + UDim2.fromOffset(30,30),
@@ -226,12 +227,12 @@ local Properties = {
 	},
 	Circle = {
 		BackgroundTransparency = 1,
-		Image = "http://www.roblox.com/asset/?id=5554831670"
+		Image = "http://www.roblox.com/asset/?id=11277866027"
 	},
 	CircleButton = {
 		BackgroundTransparency = 1,
 		AutoButtonColor = false,
-		Image = "http://www.roblox.com/asset/?id=5554831670"
+		Image = "http://www.roblox.com/asset/?id=11277866027"
 	},
 	Frame = {
 		BackgroundTransparency = 1,
@@ -269,13 +270,13 @@ local Properties = {
 		Name = "More",
 		AutoButtonColor = false,
 		BackgroundTransparency = 1,
-		Image = "http://www.roblox.com/asset/?id=5555108481",
+		Image = "http://www.roblox.com/asset/?id=11277885605",
 		Size = UDim2.fromOffset(20,20),
 		Position = UDim2.fromScale(1,0.5) - UDim2.fromOffset(25,10)
 	},
 	NavBar = {
 		Name = "SheetToggle",
-		Image = "http://www.roblox.com/asset/?id=5576439039",
+		Image = "http://www.roblox.com/asset/?id=11277903227",
 		BackgroundTransparency = 1,
 		Size = UDim2.fromOffset(20,20),
 		Position = UDim2.fromOffset(5,5),
